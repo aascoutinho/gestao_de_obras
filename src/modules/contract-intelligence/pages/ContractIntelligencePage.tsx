2e8716751bfb3d98e5f83ad04adaca9996cf4318
Antonio Augusto da Silva <antonioaugustomg@hotmail.com>
2026-07-01 16:16:26 -0300

    hotfix botão rdo 01/07-16:16

--- src/modules/contract-intelligence/pages/ContractIntelligencePage.tsx
+++ src/modules/contract-intelligence/pages/ContractIntelligencePage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useProjectStore, useProjectActions, useSelectedProject } from '../../../stores/projectStore';
-import { useTeamStore } from '../../../stores/teamStore';
-import { useRdoStore } from '../../../stores/rdoStore';
+import { useTeamStore, useTeamActions } from '../../../stores/teamStore';
+import { useRdoStore, useRdoActions } from '../../../stores/rdoStore';
+import { useUiActions } from '../../../stores/uiStore';
 import CoreContractIntelligencePage from '../../../../components/ContractIntelligence/ContractIntelligencePage';
 
 export const ContractIntelligencePage: React.FC = () => {
@@ -11,6 +12,22 @@ export const ContractIntelligencePage: React.FC = () => {
   const rdos = useRdoStore((state) => state.rdos);
   const { setSelectedProject } = useProjectActions();
 
+  const { setActiveMenu, setCurrentView } = useUiActions();
+  const { setSelectedTeam } = useTeamActions();
+  const { setCurrentRDO } = useRdoActions();
+
+  const handleNavigateToRDO = (rdoId: string, teamId: string) => {
+    const rdo = rdos.find((r) => r.id === rdoId);
+    const team = teams.find((t) => t.id === teamId);
+    
+    if (rdo && team) {
+      setSelectedTeam(team);
+      setCurrentRDO(rdo);
+      setCurrentView('RDO_LIST');
+      setActiveMenu('PROJECTS');
+    }
+  };
+
   return (
     <CoreContractIntelligencePage
       projects={projects}
@@ -18,6 +35,7 @@ export const ContractIntelligencePage: React.FC = () => {
       rdos={rdos}
       selectedProject={selectedProject}
       onSelectProject={setSelectedProject}
+      onNavigateToRDO={handleNavigateToRDO}
     />
   );
 };
